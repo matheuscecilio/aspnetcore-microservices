3c29f51fca56e8c0e7b759090294c2505bc357c6
Matheus Alberto <maatheusaalberto@gmail.com>
2022-01-24 20:23:32 -0300

    Register services application and create API

---
+++ src/Services/Ordering/Ordering.Application/Installers/ApplicationInstaller.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application.Installers
+{
+    public static class ApplicationInstaller
+    {
+        public static void InstallApplicationServices(this IServiceCollection services)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            services.AddAutoMapper(executingAssembly);
+            services.AddValidatorsFromAssembly(executingAssembly);
+            services.AddMediatR(executingAssembly);
+
+            services.AddTransient(
+                typeof(IPipelineBehavior<,>),
+                typeof(UnhandledExceptionBehaviour<,>)
+            );
+
+            services.AddTransient(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehaviour<,>)
+            );
+        }
+    }
+}
