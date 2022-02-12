c158ff9cf743d89359793ed35434caee8c69df2f
Matheus Alberto <maatheusaalberto@gmail.com>
2022-02-12 08:50:51 -0300

    Create and configure Ocelot project

---
+++ src/ApiGateways/OcelotApiGw/Installers/OcelotInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+
+namespace OcelotApiGw.Installers
+{
+    public static class OcelotInstaller
+    {
+        public static void InstallOcelot(this IServiceCollection services)
+        {
+            services.AddOcelot();
+        }
+    }
+}
