51e6762676cf0600f03dcb285f6c76d203c8cc13
Matheus Alberto <maatheusaalberto@gmail.com>
2022-02-12 18:44:51 -0300

    Shopping aggregator API + containerize

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var jsonSerializerOptions = new JsonSerializerOptions 
+            { 
+                PropertyNameCaseInsensitive = true 
+            };
+
+            return JsonSerializer.Deserialize<T>(dataAsString, jsonSerializerOptions);
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Installers/HttpClientInstaller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shopping.Aggregator.Services;
+using System;
+
+namespace Shopping.Aggregator.Installers
+{
+    public static class HttpClientInstaller
+    {
+        public static void InstallHttpClient(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            var catalogUrl = configuration["ApiSettings:CatalogUrl"];
+            var basketUrl = configuration["ApiSettings:BasketUrl"];
+            var orderingUrl = configuration["ApiSettings:OrderingUrl"];
+
+            services.AddHttpClient<ICatalogService, CatalogService>(x => 
+                x.BaseAddress = new Uri(catalogUrl)
+            );
+
+            services.AddHttpClient<IBasketService, BasketService>(x =>
+                x.BaseAddress = new Uri(basketUrl)
+            );
+
+            services.AddHttpClient<IOrderService, OrderService>(x =>
+                x.BaseAddress = new Uri(orderingUrl)
+            );
+        }
+    }
+}
